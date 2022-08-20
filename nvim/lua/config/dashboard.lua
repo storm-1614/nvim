26851c92f0eecf8810bd1bcd6ba13cdc05496544
风云雾 <storm1614@outlook.com>
2022-08-20 14:23:59 +0800

    remove

--- nvim/lua/config/dashboard.lua
+++
@@ -1,5 +0,0 @@
-  local home = os.getenv('HOME')
-  local db = require('dashboard')
-  db.preview_command = 'cowsay neovim'
-  --
-    }
--- nvim/lua/config/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
--- examples for your init.lua
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-})
