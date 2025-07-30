13a9cfdf3651cdb87d1bbd8d3f67b2913479be75
storm-1614 <storm1614@outlook.com>
2025-07-30 08:39:18 +0800

    cmp key enter fix & short neotree width

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,18 @@
+return {
+    "saghen/blink.cmp",
+    opts = {
+        keymap = {
+            preset = "enter",
+            ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+            ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+        },
+        completion = {
+            list = {
+                selection = {
+                    preselect = false,
+                    auto_insert = true,
+                },
+            },
+        },
+    },
+}
